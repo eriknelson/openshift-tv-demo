ddbbb9d18f68d57313a72f570b0465a33bbddc37
Erik Nelson <erik@nsk.io>
2021-04-21 20:38:54 -0400

    Fix restore script

--- 02-Stage-Pods/02-stage-pod-restore.sh
+++ 02-Stage-Pods/02-stage-pod-restore.sh
@@ -4,9 +4,7 @@ img=$(cat current_image)
 
 echo "$img"
 
-oc patch migrationcontroller/migration-controller \
+oc patch migrationcontroller migration-controller \
   -n openshift-migration \
   --type merge \
   -p '{ "spec": {"migration_stage_image_fqin": "'$img'" }}'
-
-rm current_image
